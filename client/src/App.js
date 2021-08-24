ad6d4b68313ef47532d358db89b22a754de06946
Rachel0808 <janeracheaul@gmail.com>
2021-08-24 15:21:56 +0700

    edit the table data

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import logo from "./logo.svg";
 import "./App.css";
 import TableData from "./components /TableData";
@@ -5,6 +6,19 @@ import "antd/dist/antd.css";
 
 function App() {
   return <TableData />;
+=======
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/antd.css";
+import SideBar from "./components/SideBar";
+
+function App() {
+  return (
+    <>
+      <SideBar />
+    </>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App;
---
+++ client/src/components/SideBar.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "antd/dist/antd.css";
+import { Layout, Menu, Breadcrumb, Tabs } from "antd";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  EnvironmentOutlined,
+  UserOutlined,
+  AlignLeftOutlined,
+} from "@ant-design/icons";
+import DasBoardPage from "../DasBoardPage";
+import TableData from "../components/TableData";
+const { Header, Content, Footer, Sider } = Layout;
+const SideBar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const onCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
+  return (
+    <>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1"></Menu.Item>
+
+            <Menu.Item icon={<EnvironmentOutlined />} key="3">
+              Điểm Đăng Ký
+            </Menu.Item>
+            <Menu.Item icon={<UserOutlined />} key="3">
+              Thành Viên
+            </Menu.Item>
+            <Menu.Item icon={<AlignLeftOutlined />} key="1">
+              Thống Kê
+            </Menu.Item>
+
+            <Menu.Item key="9" icon={<UserOutlined />}>
+              Thông Tin Tài Khoản
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: "0 16px" }}>
+            {/* <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb> */}
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              <DasBoardPage />
+            </div>
+            <TableData />
+          </Content>
+
+          <Footer style={{ textAlign: "center" }}>
+            SOS DashBoard Created by SOS team
+          </Footer>
+        </Layout>
+      </Layout>
+    </>
+  );
+};
+
+export default SideBar;
